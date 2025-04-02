e25d1f95cce0c686da4e8ff55ffbba7798c24e9f
nbiss <hitrunplay@gmail.com>
2025-04-01 21:08:03 -0700

    fix nbiss/ clean up

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 const box = document.getElementById('circle')
 const circle = box.getBoundingClientRect()
 
-let posX = 0
-let posY = 0
-let velX = 10
-let velY = 10
-let speed = 20
+
 let maxWidth  = window.screen.width - circle.width
 let maxHeight = window.screen.height - circle.height
 let RADIUS = circle.width
 
 
-// interval = setInterval(()=>{console.log(getRndInteger(100, 1700))}, 1000)
 
 function updatePosition(){
     posX = getRndInteger(RADIUS, maxWidth)
@@ -28,6 +23,5 @@ function getRndInteger(min, max) {
 }
 
 
-//event listeners
 const circle_button = document.getElementById('circle_button')
 circle_button.addEventListener("mouseenter", updatePosition)
